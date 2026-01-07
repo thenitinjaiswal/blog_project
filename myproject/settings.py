8c9770c074b2207fb10d5cd6f7e1775617c99f2b
Nitin Jaiswal <nitinjaiswal242004@gmail.com>
2026-01-07 10:25:51 +0530

    Fix static files (app-level static + whitenoise)

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -116,12 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "assets"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL ='media/'
-STATIC_ROOT = BASE_DIR/'assets'
+
 MEDIA_ROOT = BASE_DIR/'media'
-STATICFILES_DIRS=[
-    BASE_DIR/'static'
-]
 
 
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
